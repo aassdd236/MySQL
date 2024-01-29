eddb64e000c8f3cd1d1b9d592d787f19f81feda6
aassdd236 <aassdd236@naver.com>
2024-01-29 16:34:07 +0900

    7번 빼고

--- Practice/Practice05.sql
+++ Practice/Practice05.sql
@@ -74,8 +74,8 @@ where e.department_id=d.department_id and
 
 -- 7 sssssssssss
 select e.employee_id 사번,
-      e.last_name 성,
-      e.first_name 이름,
+       e.last_name 성,
+       e.first_name 이름,
        j.job_title 업무명,
        e.salary 월급,
        avg(e.salary) 부서평균월급,
@@ -85,15 +85,22 @@ join jobs j on e.job_id=j.job_id
 group by e.department_id, j.job_id
 ;
 
--- 8 부서 중 최대값 1개 뽑기
+-- 8
 select d.department_name 부서명,
-      avg(e.salary) avgSalary
+       s.sal avgSalary
 from employees e
 join departments d on e.department_id=d.department_id
-group by d.department_id, d.department_name
+join (select e.department_id, avg(salary) sal from employees e, departments d
+		where e.department_id=d.department_id group by department_id) s
+        on e.department_id=s.department_id
+group by e.department_id
+having s.sal >= (select max(sal) from employees e, (select avg(salary) sal, department_id
+											from employees
+											group by department_id) s
+						where e.department_id = s.department_id)
 ;
 
--- 9 목록 중 최대값 1개 뽑기
+-- 9
 select r.region_name,
        avg(e.salary) avgSalary
 from employees e
@@ -101,7 +108,9 @@ join departments d on e.department_id=d.department_id
 join locations l on d.location_id=l.location_id
 join countries c on l.country_id=c.country_id
 join regions r on c.region_id=r.region_id
-group by r.region_id;
+group by r.region_id
+order by avgSalary desc
+limit 0, 1;
 
 
 -- 10
